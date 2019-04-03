6d04fcf07d4fff3b209aa6b60d09c5b48f007cb6
abcwow <abcwow@yeah.net>
2019-04-03 17:31:55 +0800

    add judge table

--- judge_table.go
+++ judge_table.go
@@ -5,18 +5,59 @@ type JudgeItem struct {
 
 //////////////////////////
 
+type JudgeItem struct {
+	enumidx int //current op idx
+	op      OP  //prev op info
+}
+
+func (d *JudgeItem) ToString() string {
+
+	opident := op.Identity()
+
+	str := fmt.Sprintf("op%d", d.enumidx) + "_" + opident
+
+	return str
+
+}
+
+/////////////////////////////////////
+
+const (
+	REVERSE int = 0
+	NEXT    int = 1
+)
+
 type JudgeTable struct {
-	items []JudgeItem
+	ops map[string]bool
 }
 
 func NewJudgeTalbe() *JudgeTable {
 
 }
 
-func (*JudgeTable) Add() {
+func (m *JudgeTable) Add(item *JudgeItem) {
 
 }
 
-func (*JudgeTable) Find() {
+func (m *JudgeTable) Find(item *JudgeItem) bool {
+
+}
+
+func (m *JudgeTable) Judge(cur, prev OP) int {
+	item := JudgeItem{cur.enumidx, prev}
+
+	if m.Find(&item) == true {
+		return REVERSE
+	}
+
+	m.Add(&item)
+
+	return NEXT
+
+}
+
+var m_JudgeTable *JudgeTable
+
+func init() {
 
 }
--- ops.go
+++ ops.go
@@ -3,6 +3,32 @@ package main
 type OP struct {
 	enumidx int
 	cur     EnumVar
+	origin  *OPS
+}
+
+func (op *OP) Identity() string {
+
+	var ident string = ""
+
+	ident += fmt.Sprintf("op%d", op.opx)
+
+	ops := op.origin
+	var enums string
+	for i, v := range ops.cups {
+		if v.id == op.to.id {
+			enums += fmt.Sprintf("_%d", op.to.current)
+		} else if v.id == op.from.id {
+			enums += fmt.Sprintf("_%d", op.from.current)
+		} else {
+			enums += fmt.Sprintf("_%d", v.cups[i].current)
+		}
+
+	}
+
+	ident += enums
+
+	return ident
+
 }
 
 ///////////////////////////
@@ -12,39 +38,47 @@ type OPS struct {
 	ops  []OP
 }
 
-func (m *OPS) NewOps(prev OP) *OPS {
+func (m *OPS) Clone() *OPS {
 
 	var ops OPS
 
 	ops.cups = make(CUP, 0, len(m.cups))
 	copy(ops.cups, m.cups)
 
-	// update to current state of all cups
-	for i, v := range ops.cups {
+	return &ops
+}
 
-		if v.id == prev.to.id {
-			ops.cups[i] = prev.to
+func (m *OPS) Do(op *OP) {
+	// update to current state of all cups
+	for i, v := range m.cups {
+
+		if v.id == op.to.id {
+			m.cups[i] = op.to
+		}
+
+		if op.to.id == op.to.from { //do on cup itself
+			continue
 		}
 
 		if v.id == prev.from.id {
-			ops.cups[i] = prev.from
+			m.cups[i] = op.from
 		}
 	}
 
-	return &ops
 }
 
 func (m *OPS) CalcBranches(prev OP) *OPS {
 
-	d := m.NewOps(prev)
+	d := m.Clone()
+	d.Do(prev)
 
 	for i, opx := range m_EnumSetting.forms {
 
 		for _, cup1 := range d.cups {
 			for _, cup2 := range d.cups {
-				before := EnumVar{cup1, cup2}
+				before := EnumVar{cup1, cup2, &d}
 
-				after, err := opx.enum(after)
+				after, err := opx.enum(before)
 				if err != nil {
 					continue
 				}
